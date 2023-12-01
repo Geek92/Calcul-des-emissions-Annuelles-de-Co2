9db981eb76536fa0f8beede62aacc67b30705f52
patrick tchossiewe <patrickfrank.tchossiewedjengoue.etu@univ-lille.fr>
2023-12-01 17:43:09 +0100

    correction du premier service et implementation du deuxieme service

--- projet bilan carbone/app/main.py
+++ projet bilan carbone/app/main.py
@@ -22,56 +22,112 @@ facteurs_emission = {"Avion court courrier,sans traînées":{"2022":0.1416, "202
                      "Autocar - trajets intercités":{"2021": 0.0306,"2020":0.0364},
                      "Autobus moyen grande ville":{"2021":0.137,"2019":0.146,"2014":1.9698},
                      "Autobus moyen petite ville":{"2021":0.146,"2019":0.156},
-                     "Ferry de jour":{"2019": 0.979}
+                     "Ferry de jour":{"2019": 0.979},
+                     "Voiture a motorisation essence":{"2018":0.2234},
+                     "Voiture a motorisation gazole":{"2018": 0.2121},
+                     "Voiture a motorisation moyenne":{"2018":0.2156},
+                     "Voiture a motorisation GPL":{"2018":0.2174},
+                     "Voiture a motorisation GNV":{"2018":0.2218},
+                     "Voiture a motorisation E85":{"2018":0.1467},
+                     "Voiture hybride":{"2020": 0.1828},
+                    "Voiture ellectrique":{"2020": 0.1034},
+                    "Moto >= 250 cm3 mixte":{"2018":0.1913, "2015":0.2037},
+                    "Byciclette musculaire":{"2019":0.005},
+                    "VAE, bicyclette a assistance électrique":{"2020":0.0109},
+                    "Trotinette electrique":{"2020":0.0249},
+                    "avion Essence aviation (AvGas) France continentale":{"2014":3.0113},
+                    "Helicoptere Essence aviation (AvGas) France continentale":{"2014":3.0113},
+                    "Avion Kérosène Jet A1 ou A France continentale":{"2018":3.0791},
+                    "Helicopter Kérosène Jet A1 ou A France continentale":{"2014":3.0791},
+                    "Bateau Essence - Supercarburant":{"2021":6.5051, "2016":4.4863},
+                    "HFO":{"2014":2.9329},
+                    "LFO":{"2014":3.0506},
+                    "MDO":{"2014":3.0506},
+                    "Bateau oceanographique avec couchage à bord":{"2019": 910},
+                    "Bateau oceanographique local sans couchage à bord":{"2019": 7.6}
                      }
 
-def calculer_distance_annuelle_par_mode_transport(fichier_csv):
-    '''
-    Description de la fonction: cette fonction calcule la distance annuelle parcourue pour chaque mode de transport
+def valeur_co2_correspondant_au_mode_transport(mode_transport,annee):
+    """
+    Description: retourne le facteur d'emission du moyen de transport passé en parametre.
     Args:
-    parametre1 (csv): fichier csv qui contient les informations de l'entreprise sur les deplacements professionels
-    returns:
-    dict: dictionaire contenant les distances parcourues par an et par mode de transport
-    
-    '''
-    distance_annuelle_par_transport_plus_annee = {}
-    mode_transport = ''
+    mode_transport(str): mode transport.
+    annee (int): l'anné pour laquelle on desire récuperer le facteur d'émission.
+    Returns:
+    int : facteur d'émission du mode de transport.
+    """
+    valeur_co2_facteur_emission = 0.0
+    str_annee = str(annee)
+    for key in facteurs_emission.keys():
+        if key == mode_transport and (str_annee in facteurs_emission[mode_transport].keys()):
+            valeur_co2_facteur_emission = facteurs_emission[key][str_annee]
+            break
+    return valeur_co2_facteur_emission 
+
+
+@app.get('/get_travel_ghg_emission')
+def calculer_quantite_annuelle_de_co2_deplacements_pro():
+    """
+    Description: retourne la valeur totale de co2 groupé par année et par object de mission.
+    Args: None
+    Returns:
+    dict : un dictionnaire contenant les quantités totales de co2 émises par annéé et par ordre de mission.
+    """
+    fichier_csv = 'csv/Deplacements.csv' #fichier csv produit par le SI-Deplacement
+    mode_transport =''
     distance = 0.0
+    quantite_annuelle_de_co2_deplacements_pro = {}
     with open(fichier_csv, newline='') as csv_file:
         lecteur_csv = csv.DictReader(csv_file)
-        
         for line in lecteur_csv:
             date = datetime.strptime(line['Date'], "%d/%m/%Y")
             annee = date.year
             mode_transport = line['Mode de transport']
             distance = float(line['Distance'])
-            if annee in distance_annuelle_par_transport_plus_annee:
-                if mode_transport in distance_annuelle_par_transport_plus_annee[annee].keys():
-                    distance_annuelle_par_transport_plus_annee[annee][mode_transport] += distance
-                else:
-                    distance_annuelle_par_transport_plus_annee[annee][mode_transport] = distance
+            object_mission = line['Objet mission']
+            #on calcule la distance totale parcourue en considerant le fait que le chemin a été parcourue en allé et retour
+            distance_totale = distance * 2
+            valeur_co2 = valeur_co2_correspondant_au_mode_transport(mode_transport,annee)
+            quantité_annuelle_co2 = distance_totale * valeur_co2
+            if annee not in quantite_annuelle_de_co2_deplacements_pro.keys():
+                quantite_annuelle_de_co2_deplacements_pro[annee]= {object_mission:quantité_annuelle_co2}
             else:
-                distance_annuelle_par_transport_plus_annee[annee] = {mode_transport:distance}
-               
-    return distance_annuelle_par_transport_plus_annee
-
-
-@app.get('/co2GenereParDeplacementsPro')
-def calculer_quantite_annuelle_de_co2_induite():
-    fichier_csv = 'csv/test.csv'
-    distance_annuelle_par_transport_plus_annee = calculer_distance_annuelle_par_mode_transport(fichier_csv)
-    taux_annuel_de_co2 = 0.0
-    taux_annuel_de_co2_par_moyen_de_transport = {}
-    try:
-        for annee in distance_annuelle_par_transport_plus_annee.keys():
-            for moyen_de_transport in distance_annuelle_par_transport_plus_annee[annee].keys():
-                if ((moyen_de_transport in facteurs_emission.keys()) and (str(annee) in facteurs_emission[moyen_de_transport].keys())):
-                    taux_co2_par_moyen_de_transport = distance_annuelle_par_transport_plus_annee[annee][moyen_de_transport] * facteurs_emission[moyen_de_transport][str(annee)]
-                    if annee not in taux_annuel_de_co2_par_moyen_de_transport.keys():
-                        taux_annuel_de_co2_par_moyen_de_transport[annee] = {moyen_de_transport: taux_co2_par_moyen_de_transport}
-                    else:
-                        taux_annuel_de_co2_par_moyen_de_transport[annee][moyen_de_transport] = taux_co2_par_moyen_de_transport
+                if object_mission in quantite_annuelle_de_co2_deplacements_pro[annee].keys():
+                    quantite_annuelle_de_co2_deplacements_pro[annee][object_mission]+= quantité_annuelle_co2
+                else:
+                    quantite_annuelle_de_co2_deplacements_pro[annee][object_mission] = quantité_annuelle_co2
     
-    except:
-        print("le facteur d'émission correspondant au ",moyen_de_transport, "pour l'année ",annee,"n'est pas disponible")
-    return taux_annuel_de_co2_par_moyen_de_transport
+    return quantite_annuelle_de_co2_deplacements_pro    
+
+
+@app.get('/get_commuting_ghg_emission')
+def calculer_quantite_co2_deplacement_domicile_travavail():
+    """
+    Description: retourne la valeur totale de co2 groupé par année et par service.
+    Args: None
+    Returns:
+    dict : un dictionnaire contenant les quantités totales de co2 émises par annéé et par service.
+    """
+    fichier_csv = 'csv/RH.csv'  #fichier csv produit par le SI-RH 
+    quantite_co2_deplacement_domicile_travavail = {}
+    with open(fichier_csv, newline='') as csv_file:
+        lecteur_csv = csv.DictReader(csv_file)
+        for line in lecteur_csv:
+            annee = line['Année']
+            distance = int(line['Distance domicile travail'])
+            mode_transport = line['Mode de transport']
+            nombre_jours = int(line['Nombre de jours travaillé'])
+            service = line['Service']
+            distance_annuelle = distance * nombre_jours * 2
+            #on calcule la valeur de co2 equivalent à la distance annuelle parcourue
+            valeur_co2 = valeur_co2_correspondant_au_mode_transport(mode_transport,annee)
+            valeur_annuelle_co2 = distance_annuelle * valeur_co2
+            if annee not in quantite_co2_deplacement_domicile_travavail.keys():
+                quantite_co2_deplacement_domicile_travavail[annee] ={service:valeur_annuelle_co2}
+            else:
+                if service in quantite_co2_deplacement_domicile_travavail[annee].keys():
+                    quantite_co2_deplacement_domicile_travavail[annee][service]+=valeur_annuelle_co2
+                else:
+                    quantite_co2_deplacement_domicile_travavail[annee][service] = valeur_annuelle_co2
+    
+    return quantite_co2_deplacement_domicile_travavail
