b71a3fafb37ca5a38d306c04c046c4024ec5a021
patrick tchossiewe <patrickfrank.tchossiewedjengoue.etu@univ-lille.fr>
2023-12-10 23:48:33 +0100

    correction des fonctions qui permettent de generer les diagrammes et reorganisation du projet

--- projet bilan carbone/frontend/js/acquisitions.js
+++ projet bilan carbone/frontend/js/acquisitions.js
@@ -1,15 +1,19 @@
-fetch('http://localhost/get_travel_ghg_emission')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
+import Chart from 'chart.js/auto'
+import { createAnnualEmissionsChart } from './api'
+import { getdataFromServer } from './api'
 
-        return response.json();
-    })
-    .then(data => {
-        console.log('Data received from backend:', data);
-    })
+const url = 'http://localhost/get_travel_ghg_emission';
+//get_travel_ghg_emission  get_commuting_ghg_emission
+async function fetchDataAndCreateChart() {
+    try {
+        const data = await getdataFromServer(url);
+        createAnnualEmissionsChart(data); // Utilisez les données récupérées pour créer le graphique
+    } catch (error) {
+        console.error('Erreur lors de la récupération ou de la création du graphique :', error);
+    }
+}
 
-    .catch(error => {
-        console.error('an error occured during data acquisition:', error);
-    });
+fetchDataAndCreateChart();
+
+
+
